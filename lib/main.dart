56ce9272c07d3b28da394b074e7f7a7ae1cc829d
Appleyard Vincent <appleyardvincent@gmail.com>
2022-06-01 18:32:19 +0100

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fresh_food_ui/src/app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
     SystemUiOverlayStyle(statusBarColor: Colors.transparent);
 
